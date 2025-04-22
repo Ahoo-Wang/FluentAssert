65af44b73b3ea00c5bd19f6828ff532da19772c8
Ahoo Wang <ahoowang@qq.com>
2025-04-22 11:08:28 +0800

    feat(test): add tests and assertions for CompletableFuture and CompletionStage
    Add new test cases in `JdkConcurrentTest` to cover `CompletableFuture` and `CompletionStage`. Also, extend the `JdkConcurrent` class with new assertion methods for `CompletableFuture` and `CompletionStage`.
    
    - Added test case for `CompletableFuture`
    - Added test case for `CompletionStage`
    - Added assertion method for `CompletableFuture`
    - Added assertion method for `CompletionStage`

--- core/src/test/kotlin/me/ahoo/test/asserts/JdkConcurrentTest.kt
+++ core/src/test/kotlin/me/ahoo/test/asserts/JdkConcurrentTest.kt
@@ -1,8 +1,12 @@
 package me.ahoo.test.asserts
 
+import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.CompletableFutureAssert
 import org.assertj.core.api.FutureAssert
 import org.junit.jupiter.api.Test
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
 import java.util.concurrent.Future
 
 class JdkConcurrentTest {
@@ -11,5 +15,21 @@ class JdkConcurrentTest {
     fun `given Future when assert then FutureAssert`() {
         val value = mockk<Future<String>>()
         value.assert().assert().isInstanceOf(FutureAssert::class.java)
+    }
+
+    @Test
+    fun `given CompletableFuture when assert then CompletableFutureAssert`() {
+        val value = mockk<CompletableFuture<String>>()
+        value.assert().assert().isInstanceOf(CompletableFutureAssert::class.java)
+    }
+
+    @Test
+    fun `given CompletionStage when assert then CompletableFutureAssert`() {
+        val value = mockk<CompletionStage<String>> {
+            every {
+                toCompletableFuture()
+            } returns mockk()
+        }
+        value.assert().assert().isInstanceOf(CompletableFutureAssert::class.java)
     }
 }
